9e5ed990ecbaf236dd9a6f1f6c148c8959532f71
HOANG THE ANH <therealhoangtheanh@gmail.com>
2025-05-23 10:55:13 +0700

    Update load.py

--- src/model/load.py
+++ src/model/load.py
@@ -4,6 +4,8 @@ from torch import dtype
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
+from transformers import BitsAndBytesConfig
+
 import torch
 
 # Load the model and tokenizer.
@@ -22,7 +24,14 @@ def load_model(device: str = 'cuda', dtype: Optional[Union[str, dtype]] = torch.
     """
     #Load the model and tokenizer.
 
-    base_model = AutoModelForCausalLM.from_pretrained("unsloth/phi-4-bnb-4bit", torch_dtype=dtype, cache_dir=cache_dir, device_map=device, load_in_4bit=True)
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,  # Only this flag here
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="fp4",  # usually "nf4" or "fp4"
+        bnb_4bit_compute_dtype=torch.float16  # or bfloat16
+    )
+
+    base_model = AutoModelForCausalLM.from_pretrained("unsloth/phi-4-bnb-4bit", torch_dtype=dtype, cache_dir=cache_dir, device_map=device, quantization_config=quant_config)
     tokenizer = AutoTokenizer.from_pretrained("unsloth/phi-4-bnb-4bit", use_fast=True)
 
     model = PeftModel.from_pretrained(base_model, lora_path)
